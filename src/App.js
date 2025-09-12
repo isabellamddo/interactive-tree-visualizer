9b8a3a5d3d492df10c1e6e76a5d8bf7dd196581f
Captain Chris0 <captain.chris0.18@cnu.edu>
2025-09-12 15:16:23 -0400

    Added sample data file and input validation for csv upload

--- src/App.js
+++ src/App.js
@@ -7,19 +7,58 @@ function App() {
   console.log('D3 imported successfully:', typeof d3.select);
 
   const [file, setFile] = useState();
+  const [treeData, setTreeData] = useState(null);
+  const [error, setError] = useState('');
 
   const fileReader = new FileReader();
 
+  const validateHeaders = (csvText) => {
+    const lines = csvText.split('\n');
+    if (lines.length == 0) {
+      return { valid: false, error: 'No owner, name column headers found.' }
+    }
+
+    const headers = lines[0].split(',').map(h => h.trim());
+
+    if (headers.length !== 2) {
+      return { valid: false, error: 'Data does not have exactly 2 columns. Should have "owner,header".' }
+    }
+
+    if (!headers.includes('owner')) {
+      return { valid: false, error: 'Missing "owner" column.' }
+    }
+
+    if (!headers.includes('name')) {
+      return { valid: false, error: 'Missing "name" column.' }
+    }
+
+    return { valid: true }
+  }
+
+  // User selected a file
   const handleOnChange = (e) => {
     setFile(e.target.files[0]);
   };
 
+  // User uploaded file
   const handoleOnSubmit = (e) => {
     e.preventDefault();
 
     if (file) {
       fileReader.onload = function (event) {
         const csvOutput = event.target.result;
+
+        const validation = validateHeaders(csvOutput);
+
+        // Shows user error message if data doesn't pass validation
+        if (!validation.valid) {
+          setError(validation.error);
+          return;
+        }
+
+        // Clear errors if valid data
+        setError('');
+
       };
 
       fileReader.readAsText(file);
@@ -33,6 +72,12 @@ function App() {
         <input type={"file"} id={"csvFileInput"} accept={'.csv'} onChange={handleOnChange} />
         <button onClick={(e) => { handoleOnSubmit(e) }}>Upload</button>
       </form>
+
+      {error && (
+        <div style={{ color: 'red', margin: '10px' }}>
+          {error}
+          </div>
+      )}
     </div>
 
 
